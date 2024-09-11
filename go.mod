31babf7f7497642d76e7f2dab2730529472e6998
EvgeniyPatlan <evgeniy.patlan@percona.com>
2024-09-11 17:01:28 +0200

    TEL-52 bump golang version

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/percona/telemetry-agent
 
-go 1.22.5
+go 1.22.7
 
 require (
 	github.com/alecthomas/kong v0.9.0
--- packaging/scripts/telemetry-agent_builder.sh
+++ packaging/scripts/telemetry-agent_builder.sh
@@ -144,11 +144,11 @@ install_golang() {
     elif [ x"$ARCH" = "xaarch64" ]; then
       GO_ARCH="arm64"
     fi
-    wget https://golang.org/dl/go1.22.5.linux-${GO_ARCH}.tar.gz -O /tmp/golang1.22.5.tar.gz
-    tar --transform=s,go,go1.22.5, -zxf /tmp/golang1.22.5.tar.gz
+    wget https://golang.org/dl/go1.22.7.linux-${GO_ARCH}.tar.gz -O /tmp/golang1.22.7.tar.gz
+    tar --transform=s,go,go1.22.7, -zxf /tmp/golang1.22.7.tar.gz
     rm -rf /usr/local/go*
-    mv go1.22.5 /usr/local/
-    ln -s /usr/local/go1.22.5 /usr/local/go
+    mv go1.22.7 /usr/local/
+    ln -s /usr/local/go1.22.7 /usr/local/go
 }
 
 install_deps() {
